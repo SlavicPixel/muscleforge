b07b796a351c7f7c3369332e6fd91e85474c02fd
pixel <domi9132@gmail.com>
2024-02-29 21:03:04 +0100

    Added a register view

--- muscleforgeproject/muscleforgeproject/settings.py
+++ muscleforgeproject/muscleforgeproject/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'muscleforge.apps.MuscleforgeConfig',
+    'users.apps.UsersConfig',
     'crispy_forms',
     'crispy_bootstrap4',
     'django.contrib.admin',
